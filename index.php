deba69468ab62a04d3d9b5c96a81d8c8d892410e
Iván Eixarch <miao.org@gmail.com>
2021-09-17 23:48:39 +0200

    Disallow indexing from robots

--- index.php
+++ index.php
@@ -123,6 +123,7 @@ if ($_REQUEST['format'] == "json") {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots"  content="noindex,nofollow">
     <title>MyDDNS</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
   </head>

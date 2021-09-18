095f12e5ac231a7fe81da4a24f118cd39f264e56
Iván Eixarch <ivan@sinanimodelucro.org>
2021-09-18 02:21:21 +0200

    Added favicons

--- public_html/index.php
+++ public_html/index.php
@@ -131,6 +131,10 @@ if ($_REQUEST['format'] == "json") {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots"  content="noindex,nofollow">
     <title>MyDDNS</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
   </head>
   <body>
